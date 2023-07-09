d94ad9620717fb1a165fb2122ef6822abb02aa9f
patxu <patrickxu13@gmail.com>
2023-07-09 18:51:26 -0400

    format template file

--- template.py
+++ template.py
@@ -1,24 +1,26 @@
-'''
+"""
 Simple boilerplate for writing and testing technical interview questions.
 
 Problem Statement:
 
 
-'''
+"""
 
-class Solution():
+
+class Solution:
     def test(self):
-        inputs = [('value', False), ('value', True)]
+        inputs = [("value", False), ("value", True)]
 
         for input in inputs:
             value, expected = input
             output = self.solution(value)
             if output == expected:
-                print('Success:', input, output)
+                print("Success:", input, output)
             else:
-                print('Failure:', input, output)
+                print("Failure:", input, output)
 
     def solution(self, input):
         pass
+
 
 Solution().test()
